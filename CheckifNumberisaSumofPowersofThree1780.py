0af3af89e7dba3ff6ff3d7a4b9b58cce0890b51a
fenilgholani <fgholani1999@gmail.com>
2024-08-20 19:26:13 -0700

    Create CheckifNumberisaSumofPowersofThree1780.py

---
+++ CheckifNumberisaSumofPowersofThree1780.py
@@ -0,0 +1,11 @@
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        
+        while n!=0:
+            rem = n%3
+            if rem==2:
+                return False
+            n = n//3
+        return True
+
+        # 1, 3, 4, 9
