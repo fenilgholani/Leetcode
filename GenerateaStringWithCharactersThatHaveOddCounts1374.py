2c871e3d2f4dc8dd8401c3bcd4f750259d834da3
fenilgholani <fgholani1999@gmail.com>
2024-08-26 15:19:54 -0700

    Create GenerateaStringWithCharactersThatHaveOddCounts1374.py

---
+++ GenerateaStringWithCharactersThatHaveOddCounts1374.py
@@ -0,0 +1,9 @@
+class Solution:
+    def generateTheString(self, n: int) -> str:
+        
+        if n % 2 != 0:
+            return "a"*n
+        
+        else:
+            
+            return "a"*(n-1)+"b"
